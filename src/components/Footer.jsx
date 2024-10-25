3ebdc7f1976abbec26add658ebdd77ca104b11f9
luis1gonzalez <luis1gonzalez@hotmail.com>
2024-10-26 01:44:33 +0200

    footer fixed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ import P from '../assets/p.jpg'
 
 const Footer = () => {
   return (
-    <div className='flex justify-between px-4 bg-customGrayFooter text-sm font-bold text-gray-500 pb-6 pt-0.5'>
+    <div className='flex justify-between px-4 bg-customGrayFooter text-sm font-bold text-gray-500 pb-1 pt-0.5 fixed bottom-0 left-0 w-[100%]'>
       <div className='flex flex-col justify-center items-center h-[100%]'><img className=' w-[50px] h-[40px]' src={C} alt="" /><p>Home</p></div>
       <div className='flex flex-col justify-center items-center h-[100%]'><img className=' w-[50px] h-[40px]' src={B} alt="" /><p>Catálogos</p></div>
       <div className='flex flex-col justify-center items-center h-[100%] border-2 border-t-red-500'><img className=' w-[50px] h-[40px]' src={T} alt="" /><p>My Costco</p></div>
