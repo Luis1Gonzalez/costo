de0c5f10a91465944c0fb429b3e26ff47640115f
luis1gonzalez <luis1gonzalez@hotmail.com>
2024-10-25 19:15:18 +0200

    animation

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,10 +6,10 @@ import P from '../assets/p.jpg'
 
 const Footer = () => {
   return (
-    <div className='flex justify-between px-4 bg-customGrayFooter text-sm font-bold text-gray-500 pb-6'>
+    <div className='flex justify-between px-4 bg-customGrayFooter text-sm font-bold text-gray-500 pb-6 pt-0.5'>
       <div className='flex flex-col justify-center items-center h-[100%]'><img className=' w-[50px] h-[40px]' src={C} alt="" /><p>Home</p></div>
       <div className='flex flex-col justify-center items-center h-[100%]'><img className=' w-[50px] h-[40px]' src={B} alt="" /><p>Catálogos</p></div>
-      <div className='flex flex-col justify-center items-center h-[100%]'><img className=' w-[50px] h-[40px]' src={T} alt="" /><p>My Costco</p></div>
+      <div className='flex flex-col justify-center items-center h-[100%] border-2 border-t-red-500'><img className=' w-[50px] h-[40px]' src={T} alt="" /><p>My Costco</p></div>
       <div className='flex flex-col justify-center items-center h-[100%]'><img className=' w-[50px] h-[40px]' src={A} alt="" /><p>Tienda</p></div>
       <div className='flex flex-col justify-center items-center h-[100%]'><img className=' w-[50px] h-[40px]' src={P} alt="" /><p>Más</p></div>
     </div>
